c55e1f23110ae5fdd5103ec1e8c8834d10023192
botckov <genij.newphone@gmail.com>
2026-03-12 11:03:00 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/wirescheme/', // ← имя твоего репозитория
   server: {
     host: '0.0.0.0',
     port: 5173,
